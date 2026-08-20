731f729a6097b679165d3542f2610e67f111f8df
Mike Kirkwood-Smith <michael.kirkwood-smith3@tafensw.edu.au>
2026-08-21 09:48:42 +1000

    feat: added Exercise 2 screen with updated bottom tab nav

--- src/navigation/BottomTabNavigator.tsx
+++ src/navigation/BottomTabNavigator.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 import Exercise1Screen from "../screens/Exercise1Screen";
-
+import Exercise2Screen from "../screens/Exercise2Screen";
 
 const Tab = createBottomTabNavigator();
 
@@ -9,6 +9,7 @@ export default function BottomTabNavigator() {
     return (
         <Tab.Navigator>
             <Tab.Screen name="Exercise1" component={Exercise1Screen} />
+            <Tab.Screen name="Exercise2" component={Exercise2Screen} />
         </Tab.Navigator>
     );
 }
---
+++ src/screens/Exercise2Screen.tsx
@@ -0,0 +1,11 @@
+import { View } from "react-native";
+import { Text } from "react-native-paper";
+
+
+export default function Exercise2Screen() {
+    return (
+        <View>
+            <Text>This is Exercise 2!  🍕</Text>
+        </View>
+    );
+}
